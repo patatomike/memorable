d6a85dcb901de9bf480e3566be1cdb31d3fc7baa
Sculati Raphael <ras@8.home>
2016-03-29 14:07:09 +0200

    function set color

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,27 +11,27 @@ angular.module('memorableAppApp')
   .controller('MainCtrl', function ($scope, $http, $routeParams) {
 
     $http.get('Row1data.json').success (function(data){
-      console.log(data);
+      // console.log(data);
       var param1 = $routeParams.param1;
       $scope.item = data[param1];
-
+      console.log(getObjects(data ,'establishement_type','Coffee')); //retrieve all POI by establishement_type
+      console.log(getValues(data,'establishement_type')); //retrieve all the establishement_type
+      console.log(getKeys(data,'Coffee')); //retrieve the type of Data for a specific key (coffee in this example)
     });
 
-    var img = document.createElement('img');
-    img.setAttribute('src', 'images/post-bg.jpg')
+    setElementColorsImages('images/post-bg.jpg','blockquote',1);
+    setElementColorsImages('images/post-bg.jpg','blockquote2',0);
+    // var img = document.createElement('img');
+    // img.setAttribute('src', 'images/post-bg.jpg')
+    //
+    // img.addEventListener('load', function() {
+    //     var vibrant = new Vibrant(img);
+    //     var swatches = vibrant.swatches()
+    //     for (var swatch in swatches)
+    //         if (swatches.hasOwnProperty(swatch) && swatches[swatch])
+    //             console.log(swatch, swatches[swatch].getHex()
+    //
+    //      document.getElementById('blockquote').style.color = '#7a4426';
+    // });
 
-    img.addEventListener('load', function() {
-        var vibrant = new Vibrant(img);
-        var swatches = vibrant.swatches()
-        for (var swatch in swatches)
-            if (swatches.hasOwnProperty(swatch) && swatches[swatch])
-                console.log(swatch, swatches[swatch].getHex())
-
-        /*
-         * Results into: * Vibrant #7a4426  * Muted #7b9eae * DarkVibrant #348945 * DarkMuted #141414 * LightVibrant #f3ccb4
-         */
-
-         document.getElementById('blockquote').style.color = '#7a4426';
-    });
-
-  });
+});
---
+++ app/scripts/functions.js
@@ -0,0 +1,74 @@
+/* jshint ignore:start */
+
+//set the color of a html element based on the colors's analysis of an image
+function setElementColorsImages(url, element, color){
+  var img = document.createElement('img');
+  img.setAttribute('src', url)
+  img.addEventListener('load', function() {
+      var vibrant = new Vibrant(img);
+      var swatches = vibrant.swatches()
+      var objects = [];
+      for (var swatch in swatches) {
+          if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+              console.log(swatch, swatches[swatch].getHex())
+              objects.push(swatches[swatch].getHex());
+
+          }
+      }
+      document.getElementById(element).style.color = objects[color];
+      // console.log(objects);
+      //  Results into: * Vibrant #7a4426  * Muted #7b9eae * DarkVibrant #348945 * DarkMuted #141414 * LightVibrant #f3ccb4
+      //  document.getElementById('blockquote').style.color = '#7a4426';
+  });
+}
+
+//return an array of objects according to key, value, or key and value matching
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else
+        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+        if (i == key && obj[i] == val || i == key && val == '') { //
+            objects.push(obj);
+        } else if (obj[i] == val && key == ''){
+            //only add if the object is not already in the array
+            if (objects.lastIndexOf(obj) == -1){
+                objects.push(obj);
+            }
+        }
+    }
+    return objects;
+}
+
+//return an array of values that match on a certain key
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    return objects;
+}
+
+//return an array of keys that match on a certain value
+function getKeys(obj, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getKeys(obj[i], val));
+        } else if (obj[i] == val) {
+            objects.push(i);
+        }
+    }
+    return objects;
+}
+
+/* jshint ignore:end */
